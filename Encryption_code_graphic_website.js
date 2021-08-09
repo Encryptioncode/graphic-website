f8004c81ed65c852f342b6332284461fc1eacbe1
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-08-09 09:05:16 -0700

    Add files via upload

---
+++ Encryption_code_graphic_website.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(2000,8000);
+  //background(0,0,0);
+  fill(0,0,0);
+  rect(0,0,2000,3000);
+  rect(0,6000,2000,2000);
+  fill(200,0,0);
+  rect(2000,0,2000,3000);
+}
+
+
+function draw() {
+  fill(200,0,0);
+  rect(0,3000,2000,3000);
+}
